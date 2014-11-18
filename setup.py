5ecbf5b308adb8b99b993b18625de4b317a4dd43
Robert Vesco <robertvesco@olav-sorensons-computer.local>
2014-11-18 10:34:32 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='scopus_spider',
+      version='0.1',
+      description='Search and scrape scopus database',
+      url='',
+      author='Robert Vesco',
+      author_email='rv_spam@hotmail.com',
+      license='MIT',
+      packages=['scopus_scraping'],
+      zip_safe=False
+)
